66fb9283f18cc1bbf46c40b9439b68fc61a816ee
rohit <rohit@gmail.com>
2022-09-28 13:07:16 +0530

    project

---
+++ project1.java
@@ -0,0 +1,11 @@
+class test1
+{
+public static void main(String args[])
+{
+
+   System.out.println("hello");
+}
+
+
+
+}
---
+++ project3.java
@@ -0,0 +1,13 @@
+class test4
+{
+public static void main(String args[])
+{
+
+
+   System.out.println("i am alive");
+
+
+
+}
+
+}
